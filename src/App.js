c23718e7ccdecf29dcf38cdd7cb63266ae89f0ba
lichenyao <545295001@qq.com>
2019-05-19 23:06:48 +0800

    connect

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { addGun, removeGun, addGunAsync } from './index.redux';
 
+@connect(
+    // 你要 state 什么属性放到 props 里
+    state=>({num:state}),
+    // 你要什么方法，放到props里 自动dispatch
+    { addGun, removeGun, addGunAsync }
+)
 class App extends React.Component {
     constructor(props) {
         super(props)
     }
 
     render() {
-        const store = this.props.store
+        // const store = this.props.store
         // const num = store.getState()
         const num = this.props.num
         const addGun = this.props.addGun
@@ -25,12 +31,10 @@ class App extends React.Component {
     }
 }
 
-const mapStatetoProps = (state) => {
-    return { num: state }
-}
-
-const actionCreators = { addGun, removeGun, addGunAsync }
-
-App = connect(mapStatetoProps, actionCreators)(App)
+// const mapStatetoProps = (state) => {
+//     return { num: state }
+// }
+// const actionCreators = { addGun, removeGun, addGunAsync }
+// App = connect(mapStatetoProps, actionCreators)(App)
 
 export default App
